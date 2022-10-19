328998b26f84c230164ffdb00d27530b2100f941
vyonnelau <113070512+vyonnelau@users.noreply.github.com>
2022-10-19 14:30:29 -0400

    update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //URI: "mongodb://127.0.0.1:27017/book_store",
-  URI: "mongodb+srv://Vyonne:987oV5ZI4P2xFtZv@cluster0.fdpozff.mongodb.net/book_store",
+  //URI: "mongodb+srv://Vyonne:987oV5ZI4P2xFtZv@cluster0.fdpozff.mongodb.net/book_store",
+ URI: "mongodb+srv://Vyonne:987oV5ZI4P2xFtZv@cluster0.fdpozff.mongodb.net/book_store?retryWrites=true&w=majority",
 };
