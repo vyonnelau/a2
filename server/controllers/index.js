b613dafe68e2f549b0d9e68cc226a706f3ff168a
vyonnelau <113070512+vyonnelau@users.noreply.github.com>
2022-10-19 14:56:36 -0400

    2

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -83,8 +83,8 @@ module.exports.displayContactpage = (req, res, next) => {
 
 module.exports.displayLoginPage = (req, res, next) => {
   // check if the user is already logged in
-  console.log("======================== DISPLAY LOGIN PAGE =============================")
   if (!req.user) {
+    console.log("======================== DISPLAY LOGIN PAGE =============================")
     res.render("auth/login", {
       title: "Login",
       messages: req.flash("loginMessage"),
